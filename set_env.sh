d8de8bd4c0ca14d450a0317bf8446e88b2b654cd
Wolfgang <wlenders@gmail.com>
2023-01-29 23:19:31 +0100

    Refactor deployments and services

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=MyUdagramDB
-export POSTGRES_PASSWORD=MyUdagramDB
-export POSTGRES_HOST=myudagramdb-dev.crhownqrwnyo.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=myudagramdb
+export POSTGRES_HOST=myudagramdb.crhownqrwnyo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=MyUdagramDB
 export AWS_BUCKET=mykermitmediabucket-dev
 export AWS_REGION=us-east-1
